d068b2667088b00b9b1f611f1bd4bbe7e824e0d8
saifullah <khansaifullah1993@gmail.com>
2017-09-08 01:16:16 +0500

    comment data attributes

--- server.js
+++ server.js
@@ -206,7 +206,7 @@ io.sockets.on('connection', function(socket) {
       socket.on('sendMessage', function (data) {
           //IOS will send Room Name (not updated)
 		  logger.info(data);
-          logger.info("listening sendMessage event on server : \n "+data.messageText +"**"+  data.messageType +"**"+ data._conversationId + "**"+data._messageToMobile+"**"+data._messageToMobile);
+         // logger.info("listening sendMessage event on server : \n "+data.messageText +"**"+  data.messageType +"**"+ data._conversationId + "**"+data._messageToMobile+"**"+data._messageToMobile);
 		  console.log ("message Data on server : \n "+data.messageText +"**"+  data.messageType +"**"+ data._conversationId + "**"+data._messageToMobile+"**"+data._messageToMobile);
     var conversationMessage = new ConversationMessages();
     conversationMessage.messageType = data.messageType;
