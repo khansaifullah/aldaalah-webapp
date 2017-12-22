dc8905d2e858cdad07268ffbd9d3c958074ee99f
saifullah <khansaifullah1993@gmail.com>
2017-12-22 19:31:30 +0500

    Admin Login Set header app-awt-token

--- routes/routes.js
+++ routes/routes.js
@@ -916,7 +916,7 @@ module.exports = function(app) {
 		AdminController.login(userName,password,function (admin) {
 			
           if (admin){
-			res.setHeader("Auth-Token", "test111");
+			res.setHeader("'app-awt-token", "test111");
 			res.jsonp({status:"success",
                         message:"Successful Login",
                         object:admin});
