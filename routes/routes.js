7f58a7549583ab8118a38525b893b1d8372a07e6
saifullah <khansaifullah1993@gmail.com>
2018-01-11 02:30:18 +0500

    Allow access origin *

--- routes/routes.js
+++ routes/routes.js
@@ -46,7 +46,7 @@ module.exports = function(app) {
 	 //Enable All CORS Requests
 	app.use(cors());
 	app.use(function(req, res, next) {
-		//res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "*");
 		next();
 	  });
