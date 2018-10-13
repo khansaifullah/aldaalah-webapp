7597dddc3303d60691f5591f8bedbb770d15754a
khansaifullah <khansaifullah1993@gmail.com>
2018-10-13 21:42:29 +0500

    auth resp 1

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,11 @@ var mongoose = require('mongoose');
 
 module.exports = function (req, res, next) {
   const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('Access denied. No token provided.');
+  if (!token) 
+  //return res.status(401).send('Access denied. No token provided.');
+  return res.jsonp({status:"Failure",
+  message:"Access denied. No token provided.",
+  object:[]});
   var phoneNo;
   if (req.query.phoneNo)
     phoneNo=req.query.phoneNo;
